3c722aaf984adaad53adb76ae41b523504d2c501
hammam <ahmedhammam056@gmail.com>
2024-10-23 23:16:59 +0300

    finish splash screen

--- lib/core/routing/routing.dart
+++ lib/core/routing/routing.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
 
-class AppRoutes {
-  Route? onGenerateRoute(RouteSettings settings) {
-    final args = settings.arguments;
-    switch (settings.name) {
-      default:
-        return null;
-    }
-  }
-}
