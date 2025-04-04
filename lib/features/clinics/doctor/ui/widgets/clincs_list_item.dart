41a22f67a3d09bb2c9bf7660fe8be19f87438c81
HAMMAM\user <ahmedhammam056@gmail.com>
2025-04-04 10:03:55 +0200

    finish adding allClinics func to cubit

--- lib/features/clinics/doctor/ui/widgets/clincs_list_item.dart
+++ lib/features/clinics/doctor/ui/widgets/clincs_list_item.dart
@@ -10,7 +10,7 @@ class ClincsListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
         height: 200.h,
